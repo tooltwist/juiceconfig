aa614ee9d73bbf0ee89780395b3125689d7f9312
Philip Callender <philcal@mac.com>
2020-02-28 10:10:38 +0800

    [client] update vue-loginservice, config

--- website/protected-config/loginservice-config.js
+++ website/protected-config/loginservice-config.js
@@ -1,61 +1,34 @@
-// Load the configuration. This directory should be included in .gitignore.
-import Config from '../protected-config/websiteConfig';
 
-const hints = {
-  sitename: 'Juice',
-  register: {
-    password: true,
-    firstname: false,
-    middlename: false,
-    lastname: false,
-    resumeURL: 'http://localhost:3000/',
-    termsMessage: 'Agree to our terms?',
-    termsRoute: '/terms-and-conditions',
-  },
-  forgot: false,
-  usernames: true,
-  login: {
-    registerMessage: 'Don\'t have an account yet?',
-    email: true,
-    facebook: false,
-    github: false,
-    google: false,
-    linkedin: false,
-    twitter: false,
-  }
-}
 
 export default {
-  protocol: Config.loginservice.protocol,
-  host: Config.loginservice.host,
-  port: Config.loginservice.port,
-  version: Config.loginservice.version,
-  apikey: Config.loginservice.apikey,
-  urlPrefix: Config.loginservice.urlPrefix,
-  hints: hints,
-  // options: {
-  //   hints: {
-  //     sitename: 'Juice',
-  //     register: {
-  //       password: true,
-  //       firstname: false,
-  //       middlename: false,
-  //       lastname: false,
-  //       resumeURL: 'http://localhost:3000/',
-  //       termsMessage: 'Agree to our terms?',
-  //       termsRoute: '/terms-and-conditions'
-  //     },
-  //     forgot: false,
-  //     usernames: true,
-  //     login: {
-  //       registerMessage: 'Don\'t have an account yet?',
-  //       email: true,
-  //       facebook: false,
-  //       github: false,
-  //       google: false,
-  //       linkedin: false,
-  //       twitter: false,
-  //     }
-  //   }
-  // }
+  debug: true,
+  protocol: 'http',
+  host: 'localhost',
+  port: '9090',
+  version: 'v2',
+  apikey: 'API100ZNCYYE7FK4IIE1IWTOEBFUC',
+  // urlPrefix: Config.loginservice.urlPrefix,
+  hints: {
+    sitename: 'Juice',
+    register: {
+      password: true,
+      firstname: false,
+      middlename: false,
+      lastname: false,
+      resumeURL: 'http://localhost:3000/',
+      termsMessage: 'Agree to our terms?',
+      termsRoute: '/terms-and-conditions',
+    },
+    forgot: false,
+    usernames: true,
+    login: {
+      registerMessage: 'Don\'t have an account yet?',
+      email: true,
+      facebook: false,
+      github: false,
+      google: false,
+      linkedin: false,
+      twitter: false,
+    }
+  }
 }
