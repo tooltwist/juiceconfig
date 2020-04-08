565f22276108854cdbdee2e5bb807b515ac6c081
Sally Callender <sally.callender@tooltwist.com>
2020-04-08 17:41:21 +1000

    Updates to myaccount

--- server/src/server.js
+++ server/src/server.js
@@ -779,6 +779,68 @@ server.get('/showEnvironments', async (req, res, next) => {
   });
 }); // ******************** end of /environment server calls *********************
 
+
+// ******************** All server calls related to /myaccount page *********************
+/*
+ *  /myaccount: Select USER details from MySQL database
+ *  (See https://www.w3schools.com/nodejs/nodejs_mysql.asp)
+ */
+server.get('/myaccount', async (req, res, next) => {
+  console.log(`GET /myaccount`);
+
+  let username = req.query.userName;
+  let con = await db.checkConnection()
+  const sql = `SELECT * FROM user WHERE username =?`
+  const params = [ username ]
+  
+  con.query(sql, params, function (err, result) {
+    if (err) throw err;
+    console.log("Result: " + result);
+    res.send({ record: result[0] })
+    next()
+  });
+}); 
+
+/*
+ *  /myaccount: Select deployables for /myaccount on MySQL database
+ *  
+ */
+server.get('/usersDeployables', async (req, res, next) => {
+  console.log(`GET /usersDeployables`);
+  
+  let userID = req.query.userID
+  console.log('User id is ', userID);
+  let con = await db.checkConnection()
+  //const sql = `SELECT EU.environment, EU.user_id, EU.access, U.first_name, U.last_name FROM environment_user EU left outer join user U on EU.user_id = U.id WHERE EU.environment=?`
+  const sql = `SELECT D.name, D.owner, D.product_owner, D.description, PU.access FROM deployable D left outer join project_user PU on D.name = PU.project WHERE PU.user_id=?`
+  let params = [ userID ]
+
+  con.query(sql, params, function (err, result) {
+    if (err) throw err;
+    res.send({ deployables: result })
+    next()
+  })
+})
+
+/*
+ *  /myaccount: Select environments for /myaccount on MySQL database
+ *  
+ */
+server.get('/accountEnvironments', async (req, res, next) => {
+  console.log(`GET /accountEnvironments`);
+  
+  let userID = req.query.userID
+  let con = await db.checkConnection()
+  const sql = `SELECT E.name, E.owner, E.type, E.description, E.notes, EU.access FROM environment E left outer join environment_user EU on E.name = EU.environment WHERE EU.user_id=?`
+  let params = [ userID ]
+
+  con.query(sql, params, function (err, result) {
+    if (err) throw err;
+    res.send({ environments: result })
+    next()
+  })
+})// ************************** end of /myaccount server calls ********************************
+
 // ******************** All server calls related to /_environmentNAME page *********************
 /*
  *  /_environmentNAME: Select ENVIRONMENT values for /_environmentNAME on MySQL database
